0a7f5dc2cbc001158fbf2cfdb234078ceb4a852d
Luis Posada Cespedes <luis.posadac@perficient.com>
2021-07-16 15:40:20 -0500

    Personal Information Locators

---
+++ src/page/personal-information.page.ts
@@ -0,0 +1,75 @@
+/* eslint-disable class-methods-use-this */
+import {
+  $, $$, by, browser, ElementFinder, element, ExpectedConditions
+} from 'protractor';
+
+export class PersonalInformationPage {
+  private firstNameInput: ElementFinder;
+
+  private lastNameInput: ElementFinder;
+
+  private submitButton: ElementFinder;
+
+  constructor() {
+    this.firstNameInput = $('[name="firstname"]');
+    this.lastNameInput = $('[name="lastname"]');
+    this.submitButton = $('[name="submit"]');
+  }
+
+  public async getTitle() {
+    return $$('h1').first().getText();
+  }
+
+  private async fillName(firstName: string, lastName: string): Promise<void> {
+    await this.firstNameInput.sendKeys(firstName);
+    await this.lastNameInput.sendKeys(lastName);
+  }
+
+  private async fillSex(sex: string): Promise<void> {
+    await $(`[name="sex"][value="${sex}"]`).click();
+  }
+
+  private async fillExperience(experience: number): Promise<void> {
+    await $(`[name="exp"][value="${experience}"]`).click();
+  }
+
+  private async fillProfession(profession: string[]): Promise<void> {
+    profession.forEach(async (single_profession) => {
+      await $(`[name="profession"][value="${single_profession}"]`).click();
+    });
+  }
+
+  private async fillTools(tools: string[]): Promise<void> {
+    tools.forEach(async (tool) => {
+      await $(`[name="tool"][value="${tool}"]`).click();
+    });
+  }
+
+  private async fillContinent(continent: string): Promise<void> {
+    await element(by.name('continents')).element(by.cssContainingText('option', continent)).click();
+  }
+
+  private async fillCommands(commands: string[]): Promise<void> {
+    commands.forEach(async (command) => {
+      await element(by.name('selenium_commands')).element(by.cssContainingText('option', command)).click();
+    });
+  }
+
+  public async fillForm(data: {firstName: string, lastName: string, sex: string, experience: number,
+    profession: string[], tools: string[], continent: string,
+    commands: string[] }): Promise<void> {
+    await this.fillName(data.firstName, data.lastName);
+    await this.fillSex(data.sex);
+    await this.fillExperience(data.experience);
+    await this.fillProfession(data.profession);
+    await this.fillTools(data.tools);
+    await this.fillContinent(data.continent);
+    await this.fillCommands(data.commands);
+  }
+
+  public async submitForm() {
+    await browser.wait(ExpectedConditions.elementToBeClickable(this.submitButton), 30000);
+    await this.submitButton.click();
+    await browser.switchTo().alert().accept();
+  }
+}
---
+++ test/locators.spec.ts
@@ -0,0 +1,44 @@
+import { browser } from 'protractor';
+import {
+  PersonalInformationPage,
+} from '../src/page';
+
+describe('Enter webpage', () => {
+  beforeAll(async () => {
+    await browser.waitForAngularEnabled(false);
+    await browser.get('https://www.tutorialspoint.com/selenium/selenium_automation_practice.htm');
+  });
+  describe('Fill Form', () => {
+    beforeAll(async () => {
+      const personalInformationPage: PersonalInformationPage = new PersonalInformationPage();
+
+      await personalInformationPage.fillForm({
+        firstName: 'Alejandro',
+        lastName: 'Perdomo',
+        sex: 'Male',
+        experience: 7,
+        profession: ['Automation Tester'],
+        tools: ['Selenium Webdriver'],
+        continent: 'South America',
+        commands: [
+          'Browser Commands',
+          'Navigation Commands',
+          'Switch Commands',
+          'Wait Commands',
+          'WebElement Commands'],
+      });
+    });
+    describe('Send Information', () => {
+      beforeAll(async () => {
+        const personalInformationPage: PersonalInformationPage = new PersonalInformationPage();
+
+        await personalInformationPage.submitForm();
+      });
+      it('then should have form title', async () => {
+        const personalInformationPage: PersonalInformationPage = new PersonalInformationPage();
+
+        await expect(personalInformationPage.getTitle()).toBe('Selenium - Automation Practice Form');
+      });
+    });
+  });
+});
