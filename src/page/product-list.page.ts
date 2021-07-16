d368305939bed8842a6a01aa30c7950db8e3699f
Luis Posada Cespedes <luis.posadac@perficient.com>
2021-07-16 08:42:13 -0500

    product list

--- src/page/product-list.page.ts
+++ src/page/product-list.page.ts
@@ -1,15 +1,28 @@
 import {
-  $, ElementFinder, ExpectedConditions, browser,
+  $, $$, ElementArrayFinder, ExpectedConditions, browser, ElementFinder,
 } from 'protractor';
 
 export class ProductListPage {
+  private product_list: ElementArrayFinder;
+
   private addProductMenu: ElementFinder;
 
   constructor() {
-    this.addProductMenu = $('#center_column [title="Add to cart"]');
+    this.product_list = $$('ul.product_list');
+    this.addProductMenu = $('[id="add_to_cart"]');
   }
 
-  public async goToaddProductMenu(): Promise<void> {
+  private findByProduct(productName: string): ElementFinder {
+    return this.product_list.filter((product: ElementFinder) => product.$('.product-name').getText().then((name: string) => name.includes(productName))).first();
+  }
+
+  public async selectProduct(productName: string): Promise<void> {
+    const selectedProduct = this.findByProduct(productName);
+    await browser.wait(ExpectedConditions.elementToBeClickable(selectedProduct.$('img')), 30000);
+    await selectedProduct.$('img').click();
+  }
+
+  public async addProduct(): Promise<void> {
     await browser.wait(ExpectedConditions.elementToBeClickable(this.addProductMenu), 30000);
     await this.addProductMenu.click();
   }
