c93df2fce3eb4f01250be472a5093ecbb7eb1dc1
lposada <53833805+lposada@users.noreply.github.com>
2021-07-26 14:43:31 -0500

    More browsers (#25)
    * Saucelabs
    
    * More Browsers
    
    * fix
    
    * CI
    
    Co-authored-by: Luis Posada Cespedes <luis.posadac@perficient.com>

--- protractor/saucelabs.config.ts
+++ protractor/saucelabs.config.ts
@@ -1,26 +1,36 @@
 import { Config, browser } from 'protractor';
 import { reporter } from './helpers/reporter';
 
+const firefoxConfig = {
+  browserName: 'firefox',
+  platform: 'linux',
+  name: 'firefox-tests',
+  shardTestFiles: true,
+  maxInstances: 1,
+};
+
+const chromeConfig = {
+  browserName: 'chrome',
+  name: 'chrome-tests',
+  shardTestFiles: true,
+  maxInstances: 1,
+};
+
+const multiCapabilities = [chromeConfig, firefoxConfig];
+
 export const config: Config = {
+  multiCapabilities,
+  sauceUser: process.env.SAUCE_USERNAME,
+  sauceKey: process.env.SAUCE_ACCESS_KEY,
   framework: 'jasmine',
   specs: ['../test/**/**.spec.js'],
   SELENIUM_PROMISE_MANAGER: false,
-  getPageTimeout: 3000,
-  sauceUser: process.env.SAUCE_USERNAME,
-  sauceKey: process.env.SAUCE_ACCESS_KEY,
+  getPageTimeout: 30000,
   jasmineNodeOpts: {
     defaultTimeoutInterval: 120000,
   },
   onPrepare: () => {
     reporter();
     browser.manage().timeouts().implicitlyWait(0);
-  },
-  capabilities: {
-    name: 'UI Workshop',
-    browserName: 'chrome',
-    chromeOptions: {
-      args: ['--disable-popup-blocking', '--no-default-browser-check', '--window-size=800,600'],
-      prefs: { credentials_enable_service: false },
-    },
   },
 };
